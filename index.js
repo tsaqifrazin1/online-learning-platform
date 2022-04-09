b80d0dab498af8220f282ac8ac3b81962d462049
tsaqifrazin1 <tsaqifgojek@gmail.com>
2022-04-10 03:00:15 +0800

    chore

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const http = require("http");
 const app = require("./app");
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || 3000, () => {
+server.listen(PORT, () => {
   console.log("sukses", PORT);
 });
